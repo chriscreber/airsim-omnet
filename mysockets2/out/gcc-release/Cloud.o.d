dd1805ca19a153f103162c407ea99daeea12878d
chriscreber <clcreber1996@gmail.com>
2019-07-22 13:03:21 -0700

    Add gitignore and remove build products

--- mysockets2/out/gcc-release/Cloud.o.d
+++
@@ -1,6 +0,0 @@
-out/gcc-release//Cloud.o: Cloud.cc \
- /home/cyber/omnetpp-5.5.1/include/omnetpp.h NetPkt_m.h
-
-/home/cyber/omnetpp-5.5.1/include/omnetpp.h:
-
-NetPkt_m.h:
--- mysockets2/out/gcc-release/SocketScheduler.o.d
+++
@@ -1,9 +0,0 @@
-out/gcc-release//SocketScheduler.o: SocketScheduler.cc SocketScheduler.h \
- /home/cyber/omnetpp-5.5.1/include/omnetpp/platdep/sockets.h \
- /home/cyber/omnetpp-5.5.1/include/omnetpp.h
-
-SocketScheduler.h:
-
-/home/cyber/omnetpp-5.5.1/include/omnetpp/platdep/sockets.h:
-
-/home/cyber/omnetpp-5.5.1/include/omnetpp.h:
