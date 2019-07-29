9a1cc2eeba5a6092543e7f17839b9f7627acf91a
Chris Creber <clcreber1996@gmail.com>
2019-07-29 16:11:41 -0700

    Add AirSimCarClient

---
+++ AirSimCarClient/main.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "vehicles/car/api/CarRpcLibClient.hpp"
+#include <iostream>
+
+
+
+int main() 
+{
+    using namespace msr::airlib;
+    using namespace std;
+
+    CarRpcLibClient client;
+    client.enableApiControl(true); //this disables manual control
+    // CarControllerBase::CarControls controls;
+
+    std::cout << "Press enter to drive forward" << std::endl; std::cin.get();
+    // controls.throttle = 1;
+    // client.setCarControls(controls);
+
+    std::cout << "Press Enter to activate handbrake" << std::endl; std::cin.get();
+    // controls.handbrake = true;
+    // client.setCarControls(controls);
+
+    std::cout << "Press Enter to take turn and drive backward" << std::endl; std::cin.get();
+    /*
+    controls.handbrake = false;
+    controls.throttle = -1;
+    controls.steering = 1;
+    client.setCarControls(controls);
+    */
+
+    std::cout << "Press Enter to stop" << std::endl; std::cin.get();
+    // client.setCarControls(CarControllerBase::CarControls());
+
+    return 0;
+}
