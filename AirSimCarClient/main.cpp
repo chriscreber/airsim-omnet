fa90eaa7ea137d64f6366674654049e57706e609
Justin Nguyen <justinnuwin@gmail.com>
2019-07-29 16:16:04 -0700

    Remove comments

--- AirSimCarClient/main.cpp
+++ AirSimCarClient/main.cpp
@@ -13,26 +13,24 @@ int main()
 
     CarRpcLibClient client;
     client.enableApiControl(true); //this disables manual control
-    // CarControllerBase::CarControls controls;
+    CarApiBase::CarControls controls;
 
     std::cout << "Press enter to drive forward" << std::endl; std::cin.get();
-    // controls.throttle = 1;
-    // client.setCarControls(controls);
+    controls.throttle = 1;
+    client.setCarControls(controls);
 
     std::cout << "Press Enter to activate handbrake" << std::endl; std::cin.get();
-    // controls.handbrake = true;
-    // client.setCarControls(controls);
+    controls.handbrake = true;
+    client.setCarControls(controls);
 
     std::cout << "Press Enter to take turn and drive backward" << std::endl; std::cin.get();
-    /*
     controls.handbrake = false;
     controls.throttle = -1;
     controls.steering = 1;
     client.setCarControls(controls);
-    */
 
     std::cout << "Press Enter to stop" << std::endl; std::cin.get();
-    // client.setCarControls(CarControllerBase::CarControls());
+    client.setCarControls(CarApiBase::CarControls());
 
     return 0;
 }
